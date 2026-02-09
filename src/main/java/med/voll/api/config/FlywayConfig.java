60e6ae3686d0d0b71bebe06384b7392c937e6109
mel-ferreira <melissamouraf@gmail.com>
2026-02-09 15:12:41 -0300

    [Initial Commit] Estrutura POST concluída (receber dados via POSTMAN e armazenar em banco de dados)

---
+++ src/main/java/med/voll/api/config/FlywayConfig.java
@@ -0,0 +1,24 @@
+package med.voll.api.config;
+
+import javax.sql.DataSource;
+
+import org.flywaydb.core.Flyway;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FlywayConfig {
+
+    @Bean
+    public Flyway flyway(DataSource dataSource) {
+        Flyway flyway = Flyway.configure()
+                .dataSource(dataSource)
+                .locations("classpath:db/migration")
+                .baselineOnMigrate(true)
+                .load();
+
+        flyway.migrate();
+        return flyway;
+    }
+
+}
